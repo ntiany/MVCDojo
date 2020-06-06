8a893b5de3f298e61476e299aa5a1614f1fc4ff6
ntiany <krolkas@gmail.com>
2020-06-06 15:02:55 +0200

    initial commit

---
+++ student/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using student.Model;
+
+namespace student.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
